64780ca8c012042e980262ed215a90ae740dd5b6
ShahbazVonage <shahbaz.khan@vonage.com>
2025-07-17 12:46:56 +0530

    minor change

--- src/test/java/com/ems/service/DepartmentServiceTest.java
+++ src/test/java/com/ems/service/DepartmentServiceTest.java
@@ -5,9 +5,11 @@ import com.ems.entity.Department;
 import com.ems.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class DepartmentServiceTest {
     @Mock
     private DepartmentRepository departmentRepository;
